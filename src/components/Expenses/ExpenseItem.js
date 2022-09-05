3255375bd27a99810ed882a320d5e777dccfc5a0
rihan_a <a.rihan@live.com>
2022-09-06 01:28:26 +0200

    Adding user input componet

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,27 @@
+import React, { useState } from "react";
+
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 import Card from "../UI/Card";
 
-function ExpenseItem(props) {
+const ExpenseItem = (props) => {
+  const [title, seTitle] = useState(props.title);
+
+  const ClickHandler = function () {
+    seTitle("Rihan is Here!");
+    console.log(title);
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}> </ExpenseDate>
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price"> ${props.amount}</div>
+        <button onClick={ClickHandler}> Change Title</button>
       </div>
     </Card>
   );
-}
+};
 
 export default ExpenseItem;
